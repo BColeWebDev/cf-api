93e937cd4f7cd754a9dbc616a880e72feb4dd504
BColeWebDev <briancole1994@hotmail.com>
2023-06-07 17:27:57 -0400

    updated middlware and controllers

--- src/middleware/sorting.ts
+++ src/middleware/sorting.ts
@@ -6,11 +6,15 @@ try {
         const sorts:any = req.query.sort
         let key = sorts.split(':')[0]
         let val = sorts.split(':')[1]
-        console.log("key",key)
-        console.log("val",val)
-     return results.sort((a:any,b:any) => {
-        return val === "asc" ? a[key] >= b[key] ? 1 : -1:  a[key] >= b[key] ? -1 : 1
-    })    
+        if(val === "asc"){
+            return results.sort((a:any,b:any) => a[`${key}`].localeCompare(b[`${key}`]))
+
+        }
+        if(val === "desc"){
+            return results.sort((a:any,b:any) => b[`${key}`].localeCompare(a[`${key}`]))    
+
+        }
+
 } catch (error) {
     console.log(error)
 }
