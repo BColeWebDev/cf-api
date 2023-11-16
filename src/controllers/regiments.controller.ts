2b1c60685580fb74fff10cfefd5563bea868f7a0
BColeWebDev <briancole1994@hotmail.com>
2023-11-15 20:16:54 -0500

    all workouts

--- src/controllers/regiments.controller.ts
+++ src/controllers/regiments.controller.ts
@@ -1,87 +1,98 @@
-// Workout Plans 
+// Workout Plans
 import { Response, Request } from "express";
 import { Regiment } from "../models/regiment.model";
-import mongoose from "mongoose";
 import validation from "../config/validation";
 
+const CreateRegimentPlan = async (req: Request, res: Response) => {
+  const { name, description, userid } = req.body;
 
+  try {
+    if (
+      name === undefined ||
+      description === undefined ||
+      userid === undefined
+    ) {
+      return res.status(400).send("missing values");
+    }
+    let regiment = (await Regiment.find({ name: name })).length;
+    if (regiment > 0) {
+      return res.status(400).json({ error: "Regiment Already exist" });
+    }
+    const response = validation.createRegimentValidation(req.body);
 
-const CreateRegimentPlan =  async (req:Request,res:Response) =>{
-    const {name,description,userid} =req.body
- 
-try {
-    if(name === undefined || description === undefined || userid === undefined){
-        return res.status(400).send('missing values')
+    if (response.error! !== undefined) {
+      console.log("response", response);
+      return res.status(400).json({ error: response });
     }
-        let regiment =  (await Regiment.find({name:name})).length
-    if(regiment > 0){
-        return res.status(400).json({error:"Regiment Already exist"})
-    }
-    const response = validation.createRegimentValidation(req.body)
-
-    if(response.error! !== undefined){
-        console.log("response", response)
-        return res.status(400).json({error:response})
-    }
-    const newRegiment = await Regiment.build({name,description,userid, routines:[], isCompleted:false});
-    newRegiment.save()
+    const newRegiment = await Regiment.build({
+      name,
+      description,
+      userid,
+      routines: [],
+      isCompleted: false,
+    });
+    newRegiment.save();
     return res.json(newRegiment);
-} catch (error) {
- 
-   return  res.status(500).json(error)
-}
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
 // PUT - Update workout Plans (Regiment ID)
-const UpdateRegimentPlan =  async (req:Request,res:Response) =>{
-const {name,description, isCompleted} = req.body
+const UpdateRegimentPlan = async (req: Request, res: Response) => {
+  const { name, description, isCompleted } = req.body;
+    try {
+        
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+  const response = await Regiment.findByIdAndUpdate(req.params.id, {
+    name,
+    description,
+    isCompleted,
+  });
 
-const response  = await Regiment.findByIdAndUpdate( req.params.id,{name,description,isCompleted})
-
-return res.status(200).json(response)
-
+  return res.status(200).json(response);
 };
 // GET  - Get all Workout Plans (Regiment ID)
-const GetAllRegimentPlan = async (req:Request,res:Response) =>{
-    const{id} = req.params
-try {
-    const days =['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
-    const regimentDays = []
-    console.log("params",req.params.id)
-    const allRegiments = await  Regiment.find({userid: id})
-   
-    return res.status(200).json(allRegiments)
-} catch (error) {
-    res.status(400).json(error)
-}
+const GetAllRegimentPlan = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  if(id === undefined || id === null){
+    return res.status(400).json({error:"No ID provided"})
+  }
+  try {
+    console.log("params", req.params.id);
+    const allRegiments = await Regiment.find({ userid: id });
+
+    return res.status(200).json(allRegiments);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 // GET - Get Workout Plan (Regiment ID)
-const GetSingleRegimentPlan = async (req:Request,res:Response) =>{
-try {
-    const singleWorkout = await Regiment.findById(req.params.id)
-    return res.status(200).json(singleWorkout)
-} catch (error) {
-    res.status(400).json(error)
-}
+const GetSingleRegimentPlan = async (req: Request, res: Response) => {
+  try {
+    const singleWorkout = await Regiment.findById(req.params.id);
+    return res.status(200).json(singleWorkout);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
-
-
-
 // DELETE - Delete Regiment plans (Training ID)
-const DeleteRegimentPlan = async (req:Request,res:Response) =>{
-    try {
-        const deleteRegiment = await Regiment.findByIdAndDelete(req.params.id)
-        return res.status(200).json(deleteRegiment)
-    } catch (error) {
-        res.status(400).json(error)
-    }
+const DeleteRegimentPlan = async (req: Request, res: Response) => {
+  try {
+    const deleteRegiment = await Regiment.findByIdAndDelete(req.params.id);
+    return res.status(200).json(deleteRegiment);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 export default {
-    CreateRegimentPlan,
-    UpdateRegimentPlan,
-    GetAllRegimentPlan,
-    GetSingleRegimentPlan,
-    DeleteRegimentPlan
-}
+  CreateRegimentPlan,
+  UpdateRegimentPlan,
+  GetAllRegimentPlan,
+  GetSingleRegimentPlan,
+  DeleteRegimentPlan,
+};
