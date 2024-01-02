4e3604b7ba52ccb4d2d63f0a4e41c99ca287d909
BColeWebDev <Briancole1994@hotmail.com>
2024-01-01 19:53:32 -0500

    create training day

--- src/models/trainingdays.model.ts
+++ src/models/trainingdays.model.ts
@@ -1,50 +1,52 @@
 import { ObjectId } from "bson";
 import mongoose from "mongoose";
-import {WorkoutAttrs,Workout} from "./workout.model"
-interface TrainingDaysAttrs{
- 
-    description:string,
-    name:string,
-    day: string,
-    workouts:WorkoutAttrs[]
-    _id?:string
-
+import { WorkoutAttrs, Workout } from "./workout.model";
+interface TrainingDaysAttrs {
+  description: string;
+  name: string;
+  day: string;
+  workouts: WorkoutAttrs[];
+  _id?: string;
 }
 
-
-export interface TrainingDocument extends mongoose.Document{
-    description:string,
-    name:string,
-    day:string,
-    workouts:WorkoutAttrs[]
+export interface TrainingDocument extends mongoose.Document {
+  description: string;
+  name: string;
+  day: string;
+  workouts: WorkoutAttrs[];
 }
 
-interface TrainingDayModel  extends mongoose.Model<TrainingDocument>{
-    build(attrs:TrainingDaysAttrs): TrainingDocument
+interface TrainingDayModel extends mongoose.Model<TrainingDocument> {
+  build(attrs: TrainingDaysAttrs): TrainingDocument;
 }
 
 const trainingSchema = new mongoose.Schema({
-    description:{
-        type:String,
-        
-    },
-    name:{
-        type:String,
-        
-    },
-    day:{
-        type:String,
-     
-    },
-    workouts:{
-        type:[Workout.schema]
-    },
-    isCompleted:{
-        type:Boolean,
-        default: false
-    },
+  description: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  day: {
+    type: String,
+  },
+  workouts: {
+    type: [Workout.schema],
+  },
+  primaryMuscleGroup: {
+    type: [String],
+  },
+  secondaryMuscleGroup: {
+    type: [String],
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-})
-
-const TrainingDay = mongoose.model<TrainingDayModel,TrainingDayModel>('TrainingDay',trainingSchema)
-export {TrainingDay, TrainingDaysAttrs}
+const TrainingDay = mongoose.model<TrainingDayModel, TrainingDayModel>(
+  "TrainingDay",
+  trainingSchema
+);
+export { TrainingDay, TrainingDaysAttrs };
