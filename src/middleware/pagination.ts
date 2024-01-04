9618472e6e8214bdf8dc3734fb791db13bc64332
BColeWebDev <Briancole1994@hotmail.com>
2024-01-03 23:54:46 -0500

    limit 1000

--- src/middleware/pagination.ts
+++ src/middleware/pagination.ts
@@ -1,22 +1,18 @@
+import { Response, Request } from "express";
+import Sorting from "./sorting";
 
-import { Response, Request, } from "express"
-import Sorting from "./sorting"
+const Pagination = (req: Request, data: any) => {
+  const page: number = Number(req.query.page) ?? 1;
+  const pageDisplay: number = Number(req.query.limit) ?? 1000;
+  const startIndex = (page - 1) * pageDisplay;
+  const endIndex = page * pageDisplay;
 
-const Pagination = (req:Request, data:any) =>{
-    
-    const page:number = Number(req.query.page) ?? 1
-    const pageDisplay:number = Number(req.query.limit) ?? 10
-    const startIndex = (page - 1) * pageDisplay
-    const endIndex = page * pageDisplay
+  const results = data.slice(startIndex, endIndex);
+  if (req.query.sort !== undefined) {
+    return Sorting(req, results);
+  } else {
+    return results;
+  }
+};
 
-    const results = data.slice(startIndex,endIndex)
-    if(req.query.sort !== undefined){
-        return Sorting(req,results)
-    }else{
-        return results
-    }
-    
-}
-
-
-export default Pagination
+export default Pagination;
