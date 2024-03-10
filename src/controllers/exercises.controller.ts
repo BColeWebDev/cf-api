256c42a6d861e1b01ea55b75dfb74d4be8b15fde
BColeWebDev <Briancole1994@hotmail.com>
2024-03-10 17:31:46 -0400

    filters

--- src/controllers/exercises.controller.ts
+++ src/controllers/exercises.controller.ts
@@ -95,8 +95,9 @@ const GetAllExercises = async (req: Request, res: Response) => {
     if (sortation !== undefined) {
       items = Sorting(req, items);
     }
-
-    items = Filtering(req, items);
+    if (filters !== undefined) {
+      items = Filtering(req, items);
+    }
 
     // // Pagination Check
     let data: object[] = Pagination(req, items);
