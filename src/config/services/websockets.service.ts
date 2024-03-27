0a8a42d35ca4e0784c8cebc728fbddc1aedf7bdd
BColeWebDev <briancole1994@hotmail.com>
2024-03-27 18:11:59 -0400

    WS connection

---
+++ src/config/services/websockets.service.ts
@@ -0,0 +1,19 @@
+
+import{createServer} from "node:http"
+import { Server } from 'socket.io';
+function webSocket(app:any){
+    console.log("Workouts")
+    const server = createServer(app);
+    const io = new Server(server);
+
+    io.on('connection', (socket) => {
+        console.log(`connect ${socket.id}`);
+      
+        socket.on('disconnect', (reason) => {
+          console.log(`disconnect ${socket.id} due to ${reason}`);
+        });
+      });
+}
+export {
+    webSocket
+}
