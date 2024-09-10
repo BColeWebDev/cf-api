3d401561ecb53dca6b41eebbb16fb737c0aab63c
BColeWebDev <Briancole1994@hotmail.com>
2024-09-09 21:19:11 -0400

    initialize image config

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@
 
 import { app } from "./app";
 import db from "./db";
-import {InitializeRedis} from "./db/redis/index";
+import { InitializeRedis } from "./db/redis/index";
 import { config } from "../config";
 import { webSocket } from "./config/services/websockets.service";
+import { configConnectionImg } from "./config/services/image.service";
 const PORT = config.server.port || 8000;
 
 // WebSocket
@@ -16,6 +17,7 @@ webSocket(app);
 const start = async () => {
   db.on("error", console.error.bind(console, "MongoDB connection error:"));
   await InitializeRedis();
+  await configConnectionImg();
   // redis.on("error", console.error.bind(console, "Redis connection error:"));
   app.listen(PORT, () => {
     console.log(`| Listening on port http://localhost:${PORT}`);
